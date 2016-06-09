56bce50646ec4fe91522dd51c1dbb84f34f1b9ac
Ronu Ghoshal <RonuGhoshal@gmail.com>
2016-06-09 14:51:11 -0700

    Delete yelp.rb

--- on_the_way/config/initializers/yelp.rb
+++
@@ -1,8 +0,0 @@
-require 'yelp'
-
-Yelp.client.configure do |config|
-  config.consumer_key = "4CTs96gSbEBqdGvqxIzlWg"
-  config.consumer_secret = "wm8AgsXZLKqf1kFPRlQ8M4YW5sU"
-  config.token = "aJXp5yp-X9DAifa6tRz5GEW-kqUUeLhh"
-  config.token_secret = "m3TGuxNlyyWzdnmMgL0yrgIBJfc"
-end
